a3615add4f8c2167e08f60079f3c66038dd79628
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-05-17 17:20:46 +0200

    timing with resource

--- valuefragments/__init__.py
+++ valuefragments/__init__.py
@@ -1,7 +1,7 @@
 """identifier file for package and building namespace"""
+from .decorators import LazyProperty  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import timing_process_time  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import timing_psutil  # noqa: [W0611] # pylint: disable=E0401
-from .decorators import timing_plainpy  # noqa: [W0611] # pylint: disable=E0401
+from .decorators import timing_resource  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import timing_thread_time  # noqa: [W0611] # pylint: disable=E0401
-from .decorators import LazyProperty  # noqa: [W0611] # pylint: disable=E0401
 from .helpers import hashfile, ic  # noqa: [W0611] # pylint: disable=E0401
