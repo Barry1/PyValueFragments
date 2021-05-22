9c9bf1995084e10694df6dbdae12de669dabc297
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-05-23 01:47:00 +0200

    process and thread in contextmanager

--- valuefragments/contextmanagers.py
+++ valuefragments/contextmanagers.py
@@ -12,21 +12,26 @@ class TimingCM:
 
     # https://book.pythontips.com/en/latest/context_managers.html#implementing-a-context-manager-as-a-class
     def __init__(self):
-        self.end_cpu = None
-        self.end_wall = None
-        self.start_cpu = None
-        self.start_wall = None
+        self.end_process: float
+        self.end_thread: float
+        self.end_wall: float
+        self.start_process: float
+        self.start_thread: float
+        self.start_wall: float
         ic("Prepared to run with Timing")
 
     def __enter__(self):
         self.start_wall = time.monotonic()  # perf_counter()
-        self.start_cpu = time.process_time()
+        self.start_process = time.process_time()
+        self.start_thread = time.thread_time()
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.end_wall = time.monotonic()  # perf_counter()
-        self.end_cpu = time.process_time()
+        self.start_wall = time.monotonic()  # perf_counter()
+        self.end_process = time.process_time()
+        self.end_thread = time.thread_time()
         print(
-            f"computed {self.end_cpu-self.start_cpu} cpu seconds",
+            f"computed {self.end_process-self.start_process} process seconds",
+            f"computed {self.end_thread-self.start_thread} thread seconds",
             f"within {self.end_wall-self.start_wall} wall seconds",
         )
