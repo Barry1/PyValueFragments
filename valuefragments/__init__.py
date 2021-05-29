a618c229377630b2977b49130370a7d7cc9177d6
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-05-29 12:32:58 +0200

    afterdark

--- valuefragments/__init__.py
+++ valuefragments/__init__.py
@@ -1,4 +1,5 @@
 """identifier file for package and building namespace"""
+from .contextmanagers import TimingCM  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import LazyProperty  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import timing_process_time  # noqa: [W0611] # pylint: disable=E0401
 from .decorators import timing_psutil  # noqa: [W0611] # pylint: disable=E0401
@@ -7,6 +8,5 @@ from .decorators import timing_thread_time  # noqa: [W0611] # pylint: disable=E0
 from .helpers import backgroundme  # noqa: [W0611] # pylint: disable=E0401
 from .helpers import hashfile  # noqa: [W0611] # pylint: disable=E0401
 from .helpers import ic  # noqa: [W0611] # pylint: disable=E0401
-from .helpers import loadonecore  # noqa: [W0611] # pylint: disable=E0401
 from .helpers import loadallcores  # noqa: [W0611] # pylint: disable=E0401
-from .contextmanagers import TimingCM  # noqa: [W0611] # pylint: disable=E0401
+from .helpers import loadonecore  # noqa: [W0611] # pylint: disable=E0401
