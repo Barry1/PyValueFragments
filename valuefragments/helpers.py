3c457195fae039c4261880cfe6518b908293317a
Bastian Ebeling <bastian.ebeling@web.de>
2021-07-03 13:44:52 +0200

    Update helpers.py
    Ignored the md5-Call as it is not used for passwords

--- valuefragments/helpers.py
+++ valuefragments/helpers.py
@@ -45,7 +45,7 @@ else:
     def hashfile(filename: str, chunklen: int = 128 * 2 ** 12) -> str:
         """Return md5 hash for file."""
         with open(filename, "rb") as thefile:
-            file_hash = hashlib.md5()
+            file_hash = hashlib.md5()  # nosec
             chunk = thefile.read(chunklen)
             while chunk:
                 file_hash.update(chunk)
