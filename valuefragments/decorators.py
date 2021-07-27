86320e3eedaa537a785add16181980554f713ece
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-07-27 20:27:07 +0200

    mypy clean

--- valuefragments/decorators.py
+++ valuefragments/decorators.py
@@ -1,13 +1,19 @@
 """module holding decorators."""
+from __future__ import annotations
+
 import time
 
-# typing with the help of <https://mypy.readthedocs.io/en/stable/generics.html#declaring-decorators>
-from typing import Any, Callable, TypeVar, cast
+# typing with the help of
+# <https://mypy.readthedocs.io/en/stable/generics.html#declaring-decorators>
+from typing import Any, Callable, TypeVar  # , cast
+
+from typing_extensions import ParamSpec
 
 from .helpers import ic  # pylint: disable=E0402
 
+ParamType = ParamSpec("ParamType")
+ResultType = TypeVar("ResultType")
 FunctionTypeVar = TypeVar("FunctionTypeVar", bound=Callable[..., Any])
-GetResultType = TypeVar("GetResultType")
 InstanceObjectType = TypeVar("InstanceObjectType")
 
 try:
@@ -16,19 +22,22 @@ except ImportError:
     ic("resource is not available")
 else:
 
-    def timing_resource(func: FunctionTypeVar) -> FunctionTypeVar:
+    def timing_resource(
+        func: Callable[ParamType, ResultType]  # type: ignore[misc]
+    ) -> Callable[ParamType, ResultType]:  # type: ignore[misc]
         """Measure execution times by resource."""
         save = func.__name__
 
-        def wrapped(*args, **kwargs):
+        # type: ignore[name-defined]
+        def wrapped(*args: ParamType.args, **kwargs: ParamType.kwargs) -> ResultType:
             """This is the new implementation."""
             before = resource.getrusage(resource.RUSAGE_SELF)[:2]
             retval = func(*args, **kwargs)
             after = resource.getrusage(resource.RUSAGE_SELF)[:2]
             print(save, sum(after) - sum(before))
-            return retval
+            return retval  # type: ignore[no-any-return]
 
-        return cast(FunctionTypeVar, wrapped)
+        return wrapped  # cast(FunctionTypeVar, wrapped)
 
 
 try:
@@ -37,49 +46,58 @@ except ImportError:
     ic("psutil is not available")
 else:
 
-    def timing_psutil(func: FunctionTypeVar) -> FunctionTypeVar:
+    def timing_psutil(
+        func: Callable[ParamType, ResultType]  # type: ignore[misc]
+    ) -> Callable[ParamType, ResultType]:  # type: ignore[misc]
         """Measures execution times by psutil."""
         save = func.__name__
 
-        def wrapped(*args, **kwargs):
+        # type: ignore[name-defined]
+        def wrapped(*args: ParamType.args, **kwargs: ParamType.kwargs) -> ResultType:
             """This is the new implementation."""
             before = psutil.Process().cpu_times()
             retval = func(*args, **kwargs)
             after = psutil.Process().cpu_times()
             print(save, after - before, sum(after - before))
-            return retval
+            return retval  # type: ignore[no-any-return]
 
-        return cast(FunctionTypeVar, wrapped)
+        return wrapped  # cast(FunctionTypeVar, wrapped)
 
 
-def timing_thread_time(func: FunctionTypeVar) -> FunctionTypeVar:
+def timing_thread_time(
+    func: Callable[ParamType, ResultType]  # type: ignore[misc]
+) -> Callable[ParamType, ResultType]:  # type: ignore[misc]
     """Measures execution times by time (thread)."""
     save = func.__name__
 
-    def wrapped(*args, **kwargs):
+    # type: ignore[name-defined]
+    def wrapped(*args: ParamType.args, **kwargs: ParamType.kwargs) -> ResultType:
         """This is the new implementation."""
         before = time.thread_time()
         retval = func(*args, **kwargs)
         after = time.thread_time()
         print(save, after - before)
-        return retval
+        return retval  # type: ignore[no-any-return]
 
-    return cast(FunctionTypeVar, wrapped)
+    return wrapped  # cast(FunctionTypeVar, wrapped)
 
 
-def timing_process_time(func: FunctionTypeVar) -> FunctionTypeVar:
+def timing_process_time(
+    func: Callable[ParamType, ResultType]  # type: ignore[misc]
+) -> Callable[ParamType, ResultType]:  # type: ignore[misc]
     """Measures execution times by time (process)."""
     save = func.__name__
 
-    def wrapped(*args, **kwargs):
+    # type: ignore[name-defined]
+    def wrapped(*args: ParamType.args, **kwargs: ParamType.kwargs) -> ResultType:
         """This is the new implementation."""
         before = time.process_time()
         retval = func(*args, **kwargs)
         after = time.process_time()
         print(save, after - before)
-        return retval
+        return retval  # type: ignore[no-any-return]
 
-    return cast(FunctionTypeVar, wrapped)
+    return wrapped  # cast(FunctionTypeVar, wrapped)
 
 
 class LazyProperty(property):
@@ -96,18 +114,15 @@ class LazyProperty(property):
     # might also help. Further interesting is
     # <https://stackoverflow.com/questions/7151890#answer-7152065>
     def __init__(
-        self,
-        getterfunction: Callable[
-            [InstanceObjectType], GetResultType
-        ],  # type: ignore[reportInvalidTypeVarUse]
+        self, getterfunction: Callable[[InstanceObjectType], ResultType]
     ) -> None:
         """Initialize special attribute and rest from super."""
         attr_name = "_lazy_" + getterfunction.__name__
 
-        def _lazy_getterfunction(instanceobj: InstanceObjectType) -> GetResultType:
+        def _lazy_getterfunction(instanceobj: InstanceObjectType) -> ResultType:
             """Check if value present, if not calculate"""
             if not hasattr(instanceobj, attr_name):
                 setattr(instanceobj, attr_name, getterfunction(instanceobj))
-            return getattr(instanceobj, attr_name)
+            return getattr(instanceobj, attr_name)  # type: ignore[no-any-return]
 
         super().__init__(_lazy_getterfunction)
