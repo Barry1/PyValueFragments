8975005f0e6ad4db55bf9ebe79c6fab98d73452b
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-08-19 22:36:18 +0200

    moved test out of basic module

---
+++ valuefragments/test_helpers.py
@@ -0,0 +1,7 @@
+"""Test functions for helpers module."""
+from valuefragments.helpers import hashfile
+
+
+def test_hashfile() -> None:
+    """Pytest routine - md5sum of empty file."""
+    assert hashfile("/dev/null") == "d41d8cd98f00b204e9800998ecf8427e"
