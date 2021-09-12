c80330ee7ef222e885c3ae9aeac103e5956e178f
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-09-12 02:54:14 +0200

    explicit exported ic

--- valuefragments/__init__.py
+++ valuefragments/__init__.py
@@ -2,3 +2,4 @@
 from .contextmanagers import *  # pylint: disable=E0401,E0402 # noqa: W0401,W0611
 from .decorators import *  # pylint: disable=E0401,E0402 # noqa: W0401,W0611
 from .helpers import *  # pylint: disable=E0401,E0402 # noqa: W0401,W0611
+from .helpers import ic
