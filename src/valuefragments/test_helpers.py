b37382fcb5b58567e35d4ca4d68847cfc5b805c1
Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-08-03 08:49:03 +0200

    removed double code

--- src/valuefragments/test_helpers.py
+++ src/valuefragments/test_helpers.py
@@ -2,9 +2,6 @@
 """Test functions for helpers module."""
 from .helpers import hashfile  # pylint: disable=E0402
 
-# from helpers import hashfile  # pylint: disable=E0402
-
-
 def test_hashfile() -> None:
     """Pytest routine - md5sum of empty file."""
     assert hashfile("/dev/null") == "d41d8cd98f00b204e9800998ecf8427e"
