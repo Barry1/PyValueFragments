d970a30ce9a993bb4c0876c377d076619c2f6618
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-03-31 18:17:48 +0200

    hopefully that will solve

---
+++ src/joblib/externals/loky/process_executor.pyi
@@ -0,0 +1,6 @@
+from concurrent.futures import Executor
+from typing import Optional,Any,Callable,Tuple,Dict
+class ProcessPoolExecutor(Executor):
+    def __init__(self,max_workers:Optional[int],job_reducers:Dict,result_reducers:Dict,timeout:Optional[int],context:Any,initializer:Callable,initargs:Tuple,env:Dict)-> None:...
+    def shutdown(self, wait:bool=True, kill_workers:bool=False) -> None: ...
+
