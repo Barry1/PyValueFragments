9a21288de7e37b87bd969babd8130a1a5f75b61b
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-04-14 17:17:59 +0200

    forgot to await

--- src/valuefragments/helpers.py
+++ src/valuefragments/helpers.py
@@ -162,7 +162,7 @@ if sys.version_info >= (3, 11):
         with concurrent.futures.ThreadPoolExecutor() as pool_executor:
             return [
                 ready_task.result()
-                for ready_task in run_calls_in_executor(the_functioncalls, pool_executor)
+                for ready_task in await run_calls_in_executor(the_functioncalls, pool_executor)
             ]
 
     __all__.append("run_grouped_in_tpe")
