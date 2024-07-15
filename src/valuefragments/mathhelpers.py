72074c6b8add49bd4142511147479d65254a3352
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-07-15 13:38:36 +0200

    interpolation polynom for three pairs

--- src/valuefragments/mathhelpers.py
+++ src/valuefragments/mathhelpers.py
@@ -5,6 +5,38 @@ from typing import Callable, TypeVar
 Tparam = TypeVar("Tparam")
 
 
+def intp(
+    x: tuple[Tparam, Tparam, Tparam], y: tuple[Tparam, Tparam, Tparam]
+) -> tuple[Tparam, Tparam, Tparam]:
+    """Returns coefficients for interpolation by second order polynom along three given pairs."""
+    d: Tparam = (
+        x[0] ** 2 * x[1]
+        + x[1] ** 2 * x[2]
+        + x[2] ** 2 * x[0]
+        - x[2] ** 2 * x[1]
+        - x[1] ** 2 * x[0]
+        - x[0] ** 2 * x[2]
+    )
+    da: Tparam = y[0] * x[1] + y[1] * x[2] + y[2] * x[0] - y[2] * x[1] - y[1] * x[0] - y[0] * x[2]
+    db: Tparam = (
+        x[0] ** 2 * y[1]
+        + x[1] ** 2 * y[2]
+        + x[2] ** 2 * y[0]
+        - x[2] ** 2 * y[1]
+        - x[1] ** 2 * y[0]
+        - x[0] ** 2 * y[2]
+    )
+    dc: Tparam = (
+        x[0] ** 2 * x[1] * y[2]
+        + x[1] ** 2 * x[2] * y[0]
+        + x[2] ** 2 * x[0] * y[1]
+        - x[2] ** 2 * x[1] * y[0]
+        - x[1] ** 2 * x[0] * y[2]
+        - x[0] ** 2 * x[2] * y[1]
+    )
+    return (da / d, db / d, dc / d)
+
+
 def polyroot(coeffs: tuple[Tparam, Tparam, Tparam], val: Tparam = 0) -> Tparam:
     """Returns root of second order polynom given in its coefficients."""
     val1: Tparam = ((coeffs[1] ** 2 - 4 * coeffs[0] * (coeffs[2] - val)) / 4 / coeffs[0] ** 2) ** (
