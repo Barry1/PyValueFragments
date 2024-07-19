ac2c399ece7c296afe6ae5392c056a733907f260
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-07-19 23:18:41 +0200

    exportmodule separated

---
+++ src/valuefragments/moduletools.py
@@ -0,0 +1,26 @@
+import sys
+from types import ModuleType
+from typing import Callable, ParamSpec, TypeVar
+
+# from .helpers import ic
+_FunCallResultT = TypeVar("_FunCallResultT")
+_FunParamT = ParamSpec("_FunParamT")
+
+
+def moduleexport(
+    class_or_function: Callable[_FunParamT, _FunCallResultT]
+) -> Callable[_FunParamT, _FunCallResultT]:
+    """Adds function or class magical to module's __all__."""
+    # Following the idea from <https://stackoverflow.com/a/35710527/#:~:text=export%20decorator>
+    module: ModuleType = sys.modules[class_or_function.__module__]
+    # ic(class_or_function.__name__ + " in " + class_or_function.__module__)
+    # ic(dir(module))
+    if hasattr(module, "__all__"):
+        if class_or_function.__name__ not in module.__all__:
+            module.__all__.append(class_or_function.__name__)
+    else:
+        setattr(module, "__all__", [class_or_function.__name__])
+    # ic(dir(module))
+    # ic(module)
+    # ic(module.__all__)
+    return class_or_function
