5419c1330203536cdd0ce2e86c8a96b1bd5a642f
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-09 13:20:39 +0200

    some documentation

--- src/valuefragments/test_valuetyping.py
+++ src/valuefragments/test_valuetyping.py
@@ -1,8 +1,11 @@
+"""Version specific tests if import mechanism is working."""
+
 from sys import version_info
 
 if version_info[:2] == (3, 11):
 
     def test_pythreeeleven() -> None:
+        """In 3.11 LiteralString was introduced but TypIs not yet."""
         from valuefragments.valuetyping import LiteralString, TypeIs
 
         assert TypeIs.__module__ == "typing_extensions"
@@ -12,6 +15,7 @@ if version_info[:2] == (3, 11):
 if version_info[:2] == (3, 12):
 
     def test_pythreetwelve() -> None:
+        """In 3.12 TypeAliasType was introduced but TypIs not yet."""
         from valuefragments.valuetyping import TypeAliasType, TypeIs
 
         assert TypeIs.__module__ == "typing_extensions"
