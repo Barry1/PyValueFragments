0c940409a41cc1864593255d9319d5c8393d52da
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-09 12:36:08 +0200

    flake8 also happy - pyflakes not, will not fit

--- src/valuefragments/valuetyping.py
+++ src/valuefragments/valuetyping.py
@@ -1,6 +1,6 @@
 """Module for typing information from typing and typing_extensions."""
 
 # pylint: disable=wildcard-import,unused-wildcard-import
-from typing import *  # pyright: ignore[reportWildcardImportFromLibrary]
+from typing import *  # pyright: ignore[reportWildcardImportFromLibrary] # noqa: F401, F403
 
-from typing_extensions import *  # type: ignore[no-redef,assignment]
+from typing_extensions import *  # type: ignore[no-redef,assignment] # noqa: F401, F403
