9aed0d1260f525abc8d71fa1b50b6e9f396ca748
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-09 12:29:12 +0200

    mypy, pylint and pyright happy

--- src/valuefragments/valuetyping.py
+++ src/valuefragments/valuetyping.py
@@ -1,6 +1,6 @@
-"""module for typing Information"""
+"""Module for typing information from typing and typing_extensions."""
 
 # pylint: disable=wildcard-import,unused-wildcard-import
-from typing import *
+from typing import *  # pyright: ignore[reportWildcardImportFromLibrary]
 
 from typing_extensions import *  # type: ignore[no-redef,assignment]
