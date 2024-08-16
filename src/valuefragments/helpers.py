aa4f0e6b4b81722518c9dc407686c09cf8a07edf
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-16 12:24:32 +0200

    formatting

--- src/valuefragments/helpers.py
+++ src/valuefragments/helpers.py
@@ -430,10 +430,10 @@ if sys.version_info >= (3, 11):
 @moduleexport
 def getselectedhreflinks(
     thebaseurl: str = "https://www.goc-stuttgart.de/event-guide/ergebnisarchiv",
-    theselector: str = '//a/@href[contains(string(), "fileadmin/ergebnisse/2024")]',  # https://stackoverflow.com/q/78877951
+    theselector: str = '//a/@href[contains(string(), "fileadmin/ergebnisse/2024")]',
 ) -> list[str]:
     """Parse HTML from URL for a-href matches by XPATH"""
-    # <https://devhints.io/xpath>
+    # <https://devhints.io/xpath> <https://stackoverflow.com/q/78877951>
     thesourcehtml: requests.Response = requests.get(url=thebaseurl, timeout=(5, 10))
     # Connect Timeout 5s, 10s for transmission
     thelogger.debug(
