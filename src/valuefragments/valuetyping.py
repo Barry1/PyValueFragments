618fe24cd0c1e4a6ddcfd501a56012ae62fa7130
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-02-28 17:56:51 +0100

    moved TypeVar and TypeVarTuples

--- src/valuefragments/valuetyping.py
+++ src/valuefragments/valuetyping.py
@@ -4,3 +4,11 @@
 from typing import *  # pyright: ignore[reportWildcardImportFromLibrary] # noqa: F401, F403
 
 from typing_extensions import *  # type: ignore[no-redef,assignment] # noqa: F401, F403
+
+LastElementT = TypeVar("LastElementT")  # noqa: F405
+OtherElementsT = TypeVarTuple("OtherElementsT")  # noqa: F405
+KwargsForPrint = TypedDict(  # noqa: F405
+    "KwargsForPrint",
+    {"sep": str, "end": str, "file": IO[str], "flush": bool},  # noqa: F405
+    total=False,
+)
