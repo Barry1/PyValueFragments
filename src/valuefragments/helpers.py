dba7d6cec5177b10e3ea8643557fcd35616879d7
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-04-23 20:23:03 +0200

    ic import changed and better

--- src/valuefragments/helpers.py
+++ src/valuefragments/helpers.py
@@ -230,7 +230,9 @@ def exists_variable(varname: str) -> bool:
 
 
 try:
-    from icecream import ic # type: ignore[attr-defined]
+    #from icecream import ic # type: ignore[attr-defined]
+    #ic=__import__("icecream").ic
+    ic=__import__("icecream").icecream.IceCreamDebugger()
 except ImportError:
     # <https://stackoverflow.com/a/73738408>
     # pylint: disable-next=keyword-arg-before-vararg
