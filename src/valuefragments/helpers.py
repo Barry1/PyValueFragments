48619b5defa3c5f6c27cd5622865e5169f28eb59
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-06-03 20:07:16 +0200

    finally solved reusable_executor import

--- src/valuefragments/helpers.py
+++ src/valuefragments/helpers.py
@@ -203,7 +203,7 @@ def closeifrunningloky() -> None:
     """Check if any (loky) backend is still open and if, close."""
     try:
         # pylint: disable=import-outside-toplevel
-        from joblib.externals.loky import get_reusable_executor
+        from joblib.externals.loky.reusable_executor import get_reusable_executor
     except ModuleNotFoundError:
         pass
     else:
