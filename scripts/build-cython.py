dfe990b6c8da890f90a470292db66cc41c0e5a9c
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-07-10 00:36:33 +0200

    better?

--- scripts/build-cython.py
+++ scripts/build-cython.py
@@ -1,1 +1,10 @@
-print("Ab geht es.")
+"""Build module for Cython compilation."""
+
+
+def build() -> None:
+    """Dummy Build function to run Cython."""
+    print("Ab geht es.")
+
+
+if __name__ == "__main__":
+    build()
