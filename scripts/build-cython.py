2f7292446bf501129b7935951169902b0458ddaa
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-07-10 15:15:11 +0200

    prints removed

--- scripts/build-cython.py
+++ scripts/build-cython.py
@@ -7,23 +7,18 @@ Happy Reading!
 from Cython.Build import cythonize
 from setuptools import setup
 
-print(__import__("sys").version)
-print("Start")
-print("Cython is there")
-#
-print("Hurry up")
+# print(__import__("sys").version)
 
 
 def build() -> None:
     """Dummy Build function to run Cython."""
-    print("Ab geht es.")
+    print("Before setup.")
     setup(
         name="valuefragments",
         ext_modules=cythonize("src/valuefragments/mathhelpers.py"),  # , show_all_warnings=True),
     )
-    print("feddich")
+    print("After setup.")
 
 
 if __name__ == "__main__":
-    cythonize("src/valuefragments/mathhelpers.py")
     build()
