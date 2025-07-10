4ecd5cdfa9fcd122b653bb59899ab43f876faf77
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-07-10 15:08:37 +0200

    testing

--- scripts/build-cython.py
+++ scripts/build-cython.py
@@ -7,16 +7,23 @@ Happy Reading!
 from Cython.Build import cythonize
 from setuptools import setup
 
+print(__import__("sys").version)
+print("Start")
+print("Cython is there")
+#
+print("Hurry up")
+
 
 def build() -> None:
     """Dummy Build function to run Cython."""
     print("Ab geht es.")
     setup(
         name="valuefragments",
-        ext_modules=cythonize("src/valuefragments/mathhelpers.py", show_all_warnings=True),
+        ext_modules=cythonize("src/valuefragments/mathhelpers.py"),  # , show_all_warnings=True),
     )
     print("feddich")
 
 
 if __name__ == "__main__":
+    cythonize("src/valuefragments/mathhelpers.py")
     build()
