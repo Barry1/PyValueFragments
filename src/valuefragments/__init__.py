091e481ec23f4a4d63bf2e57b2bc682eb19a9f27
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-07-18 09:03:10 +0200

    probneed_neu

--- src/valuefragments/__init__.py
+++ src/valuefragments/__init__.py
@@ -26,7 +26,14 @@ from .helpers import (  # pylint: disable=E0401,E0402 # noqa: W0401,W0611; backg
     run_grouped,
     thread_native_id_filter,
 )
-from .mathhelpers import easybisect, intp, polyroot, probneeds, probneeds_rec
+from .mathhelpers import (
+    easybisect,
+    intp,
+    polyroot,
+    probneeds,
+    probneeds_new,
+    probneeds_rec,
+)
 from .moduletools import moduleexport
 
 # <https://docs.python.org/3/howto/logging.html#library-config>
@@ -55,6 +62,7 @@ __all__: list[str] = [
     "polyroot",
     "portable_timing",
     "probneeds",
+    "probneeds_new",
     "probneeds_rec",
     "recurse_files_in_folder",
     "run_grouped",
