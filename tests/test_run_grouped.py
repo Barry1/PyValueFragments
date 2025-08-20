9b2fc0c6c41895907d0736750c769ca97c14568c
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-08-20 13:36:26 +0200

    test_parallel improved

--- tests/test_run_grouped.py
+++ tests/test_run_grouped.py
@@ -9,33 +9,36 @@ from typing import Callable
 import pytest
 
 if sys.version_info >= (3, 11):
+    COUNT: int = 100
     from functools import partial
+    from math import pi
 
     from valuefragments.helpers import pi_for_cpu_load, run_grouped
 
     tasklist: list[Callable[[], float]] = [
-        partial(pi_for_cpu_load, 10000, 4478) for _ in range(5)
+        partial(pi_for_cpu_load, 10000, 4478) for _ in range(COUNT)
     ]
 
     @pytest.mark.asyncio
     async def test_run_grouped_thread() -> None:
         """Fake main routine for async processing."""
         assert (
-            abs(
-                sum(await run_grouped(tasklist, "thread"), -15.638000000000002)
-            )
-            <= 0.04
+            abs(sum(await run_grouped(tasklist, "thread"), -COUNT * pi))
+            <= pi / 100 * COUNT
         )
 
     @pytest.mark.asyncio
     async def test_run_grouped_ppe() -> None:
         """Fake main routine for async processing."""
-        assert sum(await run_grouped(tasklist, "ppe")) == 15.638000000000002
+        assert (
+            abs(sum(await run_grouped(tasklist, "ppe"), -COUNT * pi))
+            <= pi / 100 * COUNT
+        )
 
     @pytest.mark.asyncio
     async def test_run_grouped_tpe() -> None:
         """Fake main routine for async processing."""
         assert (
-            abs(sum(await run_grouped(tasklist, "tpe"), -15.638000000000002))
-            <= 0.04
+            abs(sum(await run_grouped(tasklist, "tpe"), -COUNT * pi))
+            <= pi / 100 * COUNT
         )
