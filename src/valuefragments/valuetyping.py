eecbe6205e73210fc27f0a8f77176404d5c170a9
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-02-12 00:11:55 +0100

    maybe I import better

--- src/valuefragments/valuetyping.py
+++ src/valuefragments/valuetyping.py
@@ -5,6 +5,35 @@ from typing import *  # type: ignore # noqa
 
 from typing_extensions import *  # type: ignore # noqa
 
+# Example without StarImports
+
+try:
+    # Sentinel=__import__(name="typing", fromlist="Sentinel").Sentinel
+    # from typing import Sentinel
+    globals().update(
+        {
+            "Sentinel": getattr(
+                __import__(name="typing", fromlist="Sentinel"), "Sentinel"
+            )
+        }
+    )
+except (ImportError, AttributeError):
+    # Sentinel=__import__(name="typing_extensions", fromlist="Sentinel").Sentinel
+    try:
+        globals().update(
+            {
+                "Sentinel": getattr(
+                    __import__(name="typing_extensions", fromlist="Sentinel"),
+                    "Sentinel",
+                )
+            }
+        )
+    except (ImportError, AttributeError):
+        print(
+            "Sentinel is not available in typing or typing_extensions. Please upgrade to Python 3.8+ or install typing_extensions."
+        )
+# globals().update({"Sentinel":getattr(__import__(name="typing_extensions", fromlist="Sentinel"),"Sentinel")})
+
 
 class KwargsForPrint(TypedDict, total=False):  # noqa: F405
     """Class for Type Checking Kwargs 2 Print"""
