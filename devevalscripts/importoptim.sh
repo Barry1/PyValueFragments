c0410b1e5e537e45f24db16abd64d10653de3c99
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-14 21:19:27 +0200

    sinnvolle Sortierung

--- devevalscripts/importoptim.sh
+++ devevalscripts/importoptim.sh
@@ -7,25 +7,25 @@ chrt -p $$
 echo "====================================================================== TIMEIT ======================================================================"
 echo "Importlib WALL"
 python3 -OO -m timeit 'from importlib.util import find_spec; find_spec("typing")'
-echo "Importlib PROCESS"
-python3 -OO -m timeit --process 'from importlib.util import find_spec; find_spec("typing")'
-echo "__import__ WALL getattr"
-python3 -OO -m timeit 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
-echo "__import__ PROCESS getattr"
-python3 -OO -m timeit --process 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
 echo "__import__ WALL"
 python3 -OO -m timeit '__import__("importlib.util",fromlist=["util"]).find_spec("typing")'
+echo "__import__ WALL getattr"
+python3 -OO -m timeit 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
+echo "Importlib PROCESS"
+python3 -OO -m timeit --process 'from importlib.util import find_spec; find_spec("typing")'
 echo "__import__ PROCESS"
 python3 -OO -m timeit --process '__import__("importlib.util",fromlist=["util"]).find_spec("typing")'
+echo "__import__ PROCESS getattr"
+python3 -OO -m timeit --process 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
 echo "====================================================================== time ======================================================================"
 echo "Importlib"
 #time python3 -OO -c 'from importlib.util import find_spec; find_spec("typing")'
 time for i in {1..50}; do python3 -OO -c 'from importlib.util import find_spec; find_spec("typing")'; done
+echo "__import__"
+time for i in {1..50}; do python3 -OO -c '__import__("importlib.util",fromlist=["util"]).find_spec("typing")'; done
 echo "__import__ getattr"
 #time python3 -OO -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
 time for i in {1..50}; do python3 -OO -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'; done
-echo "__import__"
-time for i in {1..50}; do python3 -OO -c '__import__("importlib.util",fromlist=["util"]).find_spec("typing")'; done
 echo "====================================================================== importtime ======================================================================"
 echo "Importlib"
 python3 -OO -X importtime -c 'from importlib.util import find_spec; find_spec("typing")' 2>&1 | grep importlib\.util
