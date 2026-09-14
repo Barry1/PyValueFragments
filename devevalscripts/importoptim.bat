484e209b1c0b8139d26d042b2bc99cf4d4d672a7
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-14 14:22:26 +0200

    windows

--- devevalscripts/importoptim.bat
+++ devevalscripts/importoptim.bat
@@ -1,10 +1,22 @@
-python3 -OO -m timeit "from importlib.util import find_spec; find_spec(""typing"")"
-python3 -OO -m timeit --process "from importlib.util import find_spec; find_spec(""typing"")"
-python3 -OO -m timeit "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")"
-python3 -OO -m timeit --process "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")"
-python3 -OO -m timeit "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")"
-python3 -OO -m timeit --process "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")"
-echo "====================================================================== importtime ======================================================================"
-python3 -OO -X importtime -c "from importlib.util import find_spec; find_spec(""typing"")" 
-python3 -OO -X importtime -c "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")" 
-python3 -OO -X importtime -c "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")" 
+@echo off
+rem pushd \\wsl.localhost\Debian\home\ebeling\GitHub\PyValueFragments\devevalscripts
+rem net use W: \\wsl.localhost\Debian
+set repetitions=1234
+echo ====================================================================== timeit WALL ======================================================================
+echo Importlib
+python3 -OO -m timeit --number %repetitions% "from importlib.util import find_spec; find_spec(""typing"")"
+echo __import__ getattr
+python3 -OO -m timeit --number %repetitions% "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")"
+echo __import__
+python3 -OO -m timeit --number %repetitions% "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")"
+echo ====================================================================== timeit PROCESS ======================================================================
+echo Importlib process
+python3 -OO -m timeit --process --number %repetitions% "from importlib.util import find_spec; find_spec(""typing"")"
+echo __import__ getattr process
+python3 -OO -m timeit --process --number %repetitions% "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")"
+echo __import__ process
+python3 -OO -m timeit --process --number %repetitions% "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")"
+echo ====================================================================== importtime ======================================================================
+rem python3 -OO -X importtime -c "from importlib.util import find_spec; find_spec(""typing"")" 
+rem python3 -OO -X importtime -c "getattr(__import__(""importlib.util"",fromlist=[""util""]),""find_spec"")(""typing"")" 
+rem python3 -OO -X importtime -c "__import__(""importlib.util"",fromlist=[""util""]).find_spec(""typing"")" 
