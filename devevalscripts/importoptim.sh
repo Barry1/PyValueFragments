70c430fb50176e4b90a2c74ade543b1302de88ac
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-15 08:02:51 +0200

    lokales

--- devevalscripts/importoptim.sh
+++ devevalscripts/importoptim.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "====================================================================== SETUP ======================================================================"
-sudo renice -20 $$
-chrt -p $$
-sudo chrt -r -p 70 $$
-chrt -p $$
+#sudo renice -20 $$
+#chrt -p $$
+#sudo chrt -r -p 70 $$
+#chrt -p $$
+repetitions=45678
+number=1
 echo "====================================================================== TIMEIT ======================================================================"
 echo "Importlib WALL"
+<<<<<<< Updated upstream
 python3 -OO -m timeit 'from importlib.util import find_spec; find_spec("typing")'
 echo "__import__ WALL"
 python3 -OO -m timeit '__import__("importlib.util",fromlist=["util"]).find_spec("typing")'
@@ -26,10 +29,32 @@ time for _ in {1..50}; do python3 -OO -c '__import__("importlib.util",fromlist=[
 echo "__import__ getattr"
 #time python3 -OO -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
 time for _ in {1..50}; do python3 -OO -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'; done
+=======
+python3 -OO -m timeit --number $number --repeat $repetitions 'from importlib.util import find_spec; find_spec("typing")'
+echo "__import__ WALL getattr"
+python3 -OO -m timeit --number $number --repeat $repetitions 'getattr(__import__("importlib.util",globals(),locals(),fromlist=["util"]),"find_spec")("typing")'
+echo "__import__ WALL"
+python3 -OO -m timeit --number $number --repeat $repetitions '__import__("importlib.util",globals(),locals(),fromlist=["util"]).find_spec("typing")'
+echo "Importlib PROCESS"
+python3 -OO -m timeit --number $number --repeat $repetitions --process 'from importlib.util import find_spec; find_spec("typing")'
+echo "__import__ PROCESS getattr"
+python3 -OO -m timeit --number $number --repeat $repetitions --process 'getattr(__import__("importlib.util",globals(),locals(),fromlist=["util"]),"find_spec")("typing")'
+echo "__import__ PROCESS"
+python3 -OO -m timeit --number $number --repeat $repetitions --process '__import__("importlib.util",globals(),locals(),fromlist=["util"]).find_spec("typing")'
+echo "====================================================================== time ======================================================================"
+echo "Importlib"
+#time python3 -OO -c 'from importlib.util import find_spec; find_spec("typing")'
+time for i in {1..500}; do python3 -OO -c 'from importlib.util import find_spec; find_spec("typing")'; done
+echo "__import__ getattr"
+#time python3 -OO -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")'
+time for i in {1..500}; do python3 -OO -c 'getattr(__import__("importlib.util",globals(),locals(),fromlist=["util"]),"find_spec")("typing")'; done
+echo "__import__"
+time for i in {1..500}; do python3 -OO -c '__import__("importlib.util",globals(),locals(),fromlist=["util"]).find_spec("typing")'; done
+>>>>>>> Stashed changes
 echo "====================================================================== importtime ======================================================================"
 echo "Importlib"
 python3 -OO -X importtime -c 'from importlib.util import find_spec; find_spec("typing")' 2>&1 | grep importlib\.util
 echo "__import__ getattr"
-python3 -OO -X importtime -c 'getattr(__import__("importlib.util",fromlist=["util"]),"find_spec")("typing")' 2>&1 | grep importlib\.util
+python3 -OO -X importtime -c 'getattr(__import__("importlib.util",globals(),locals(),fromlist=["util"]),"find_spec")("typing")' 2>&1 | grep importlib\.util
 echo "__import__"
-python3 -OO -X importtime -c '__import__("importlib.util",fromlist=["util"]).find_spec("typing")' 2>&1 | grep importlib\.util
+python3 -OO -X importtime -c '__import__("importlib.util",globals(),locals(),fromlist=["util"]).find_spec("typing")' 2>&1 | grep importlib\.util
