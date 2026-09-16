73eb090db40a71db999916e91e0efc87784cddee
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-16 21:30:58 +0200

    awk eval

--- devevalscripts/importoptim.sh
+++ devevalscripts/importoptim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo ========== SETUP ==========
-sudo renice -18 $$
+repetitions=123
+evalcmd = python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
+#evalcmd = awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\nStd Dev: " sd}'
+#sudo renice -18 $$
 #chrt -p $$
 #sudo chrt -r -p 70 $$
 #chrt -p $$
-repetitions=1234
 echo ========== import ==========
 for _ in $(seq 1 $repetitions)
 do python3 << EOF
@@ -15,7 +17,7 @@ find_spec("typing")
 duration+=time.process_time_ns();
 print(duration)
 EOF
-done | python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
+done | awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\tStd Dev: " sd}'
 echo ========== __import__ ==========
 for _ in $(seq 1 $repetitions)
 do python3 << EOF
@@ -25,7 +27,7 @@ __import__("importlib.util",globals(),locals(),fromlist=["util"]).find_spec("typ
 duration+=time.process_time_ns();
 print(duration)
 EOF
-done | python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
+done | awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\tStd Dev: " sd}'
 echo ========== __import__ + getattr ==========
 for _ in $(seq 1 $repetitions)
 do python3 << EOF
@@ -35,4 +37,5 @@ getattr(__import__("importlib.util",globals(),locals(),fromlist=["util"]),"find_
 duration+=time.process_time_ns();
 print(duration)
 EOF
-done | python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
+done | awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\tStd Dev: " sd}'
+
