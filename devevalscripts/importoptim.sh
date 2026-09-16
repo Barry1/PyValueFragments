bec673abd48b1263c9fe9bfb3ed001af4d65a9c7
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-16 23:55:46 +0200

    reicht

--- devevalscripts/importoptim.sh
+++ devevalscripts/importoptim.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo ========== SETUP ==========
-repetitions=123
-evalcmd = python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
-#evalcmd = awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\nStd Dev: " sd}'
+repetitions=12345
+echo $repetitions repetitions
+#evalcmd = python3 -c"from fileinput import input; print(sum(map(int, input()))/$repetitions)"
+#evalcmd="awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print \"Mean: \" mean \"\tStd Dev: \" sd}'"
+#echo $evalcmd
 #sudo renice -18 $$
 #chrt -p $$
 #sudo chrt -r -p 70 $$
@@ -38,4 +40,26 @@ duration+=time.process_time_ns();
 print(duration)
 EOF
 done | awk '{sum+=$1; ssq+=$1^2} END {mean=sum/NR; sd=(ssq/NR - mean^2)^0.5; print "Mean: " mean "\tStd Dev: " sd}'
+#========== SETUP ==========
+# 1234
+#========== import ==========
+#Mean: 1.45871e+06	Std Dev: 108186
+#========== __import__ ==========
+#Mean: 1.46049e+06	Std Dev: 114741
+#========== __import__ + getattr ==========
+#Mean: 1.46202e+06	Std Dev: 117371
+#
+#real	7m5,676s
+#user	3m56,485s
+#sys	3m27,012s
 
+#========== SETUP ==========
+#12345 repetitions
+#========== import ==========
+#Mean: 1.46436e+06	Std Dev: 112609
+#========== __import__ ==========
+#Mean: 1.46568e+06	Std Dev: 90738.9
+#========== __import__ + getattr ==========
+#Mean: 1.4637e+06	Std Dev: 94193.5
+#2366.60user 2152.96system 1:12:05elapsed 104%CPU (0avgtext+0avgdata 9888maxresident)k
+#0inputs+32outputs (52major+637798600minor)pagefaults 0swaps
