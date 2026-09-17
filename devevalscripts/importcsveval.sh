39aa1f516879c395b91473ae364e0bc011d599f0
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-17 12:26:06 +0200

    small script for evaluation

---
+++ devevalscripts/importcsveval.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#https://medium.com/@rakeshsharma.pr/data-science-on-the-command-line-121fd9922642
+#sudo nala install csvkit
+#csvclean --enable-all-checks importoptim.csv
+csvstat importoptim.csv
